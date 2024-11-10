d878caf1aa28327d7d423d4bd305558cbb5ed1e1
tatsuya <ta282ji@icloud.com>
2024-11-10 20:32:47 +0900

    django_first-setting

---
+++ redace_django/map3d/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# from djgeojson.views import GeoJSONLayerView
+# from spectra.models import Spectrum
+
+app_name = 'map3d'
+urlpatterns = [
+    path('', views.map.default, name='default'),
+    path('db/', views.api_db.db, name='db'),
+    path('dir/', views.api_dir.dir, name='dir'),
+    path('reflectance/', views.api_reflectance.reflectance, name='reflectance'),
+    # path('jump', views.map.jump, name='jump'),
+    # path('gis', views.map.test_gis, name='gis'),
+    # path('open', GeoJSONLayerView.as_view(model=Spectrum), name='open')
+]
---
+++ redace_django/map3d/views/__init__.py
@@ -0,0 +1,4 @@
+from .map import default
+from .api_db import db
+from .api_dir import dir
+from . api_reflectance import reflectance
