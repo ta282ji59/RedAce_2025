44e4eeaaf8a46a013bfdbc3a3750824eb5f66e89
ta282ji <ta282ji@icloud.com>
2025-01-01 13:37:37 +0900

    OAuth setting

--- redace_jupyter/jupyterhub_config.py
+++ redace_jupyter/jupyterhub_config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 from jupyterhub.spawner import SimpleLocalProcessSpawner
 
 # OAuth2 Authenticator
-address = 'http://192.168.1.53'
+address = 'http://192.168.1.53:'
 c.JupyterHub.authenticator_class = 'oauthenticator.generic.GenericOAuthenticator'
 c.GenericOAuthenticator.oauth_callback_url = (address + '7010/hub/oauth_callback')
-c.GenericOAuthenticator.client_id = 'BKesCL8YzabkWcxysnO946pSZMyPZAnlHaOGolId'
-c.GenericOAuthenticator.client_secret = 'sVTYuHODYQLo9vrm7uUMvZj1E5dBG2nR9LbOVFenJjTqsPV7b2BkmdXfNY7CkeLkd021B53bGRLcdZs3uV9KcIFxPpfpr5NLs9LS1QnLLjvqH5ahold92Y8ePvSo5cK2'
+c.GenericOAuthenticator.client_id = 'qKTOI7meVX6jKvJ9cE3rf9lRvOitUbNh5JHVQoe9'
+c.GenericOAuthenticator.client_secret = 'ry7tg7edXNe2QLuWIKIGDn6io4PT50fiicFW31H5vx98jAcOPQ67eC2AfRJ6QNkYaGG6FPyYMLGWugWKMhqSvPwoPNL57qFWKP8fhfGogT7BgGyIUz9qX50c0x99yylT'
 c.GenericOAuthenticator.authorize_url = (address + '88/o/authorize/')
 c.GenericOAuthenticator.token_url = (address + '88/o/token/')
 c.GenericOAuthenticator.userdata_url = (address + '88/userdata/')
